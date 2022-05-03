dd6ebfa3b1268c88ccede7e2c6d520fb0b772e7c
riggedCoinflip <tikalker@gmail.com>
2022-05-03 03:33:06 +0200

    create requirements.txt

--- src/install-requirements.py
+++ src/install-requirements.py
@@ -1,8 +1,10 @@
 import subprocess
 from definitions import ROOT_DIR
 
+
 def install():
     subprocess.run("pip install -r requirements.txt", cwd=ROOT_DIR, shell=True)
+
 
 if __name__ == '__main__':
     install()
